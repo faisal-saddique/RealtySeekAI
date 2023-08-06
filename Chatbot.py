a005a27995b186fe6af456cab43a05c6f92681a7
faisal-saddique <faisalsaddique7964@gmail.com>
2023-08-06 05:47:14 +0500

    added logo

--- Chatbot.py
+++ Chatbot.py
@@ -58,7 +58,7 @@ class CustomDataChatbot:
 
             utils.display_msg(user_query, 'user')
 
-            with st.chat_message("assistant",avatar="assets/boom.png"):
+            with st.chat_message("assistant",avatar="https://i.imgur.com/WQoo0Np.png"):
                 st_cb = StreamHandler(st.empty())
                 response = qa_chain.run(user_query, callbacks=[st_cb])
                 st.session_state.messages.append({"role": "assistant", "content": response})
